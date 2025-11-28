0fb810b76bca06215bef4da660754125450d61ac
Christian Nuss <christian.nuss@gmail.com>
2025-11-28 11:35:24 -0500

    add missing bitcoin cmd

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+// main prints a friendly greeting using the quote package.
+func main() {
+	fmt.Println(Hello())
+}
 
 func Hello() string {
 	return quote.Hello()
